d7cd45b2d26986dbf5d2b159bbacd50a79f449a8
Alisa Kolot <kolot.alisa@gmail.com>
2021-02-23 08:46:30 -0600

    pytest study 1 rev

--- 02_22/pytest_study_1.py
+++ 02_22/pytest_study_1.py
@@ -1,1 +1,37 @@
-pytest_study_1.py
+# Test 1.
+
+# def func(x):
+#     return x +  1
+
+# def test_answer():
+#     assert func(3) ==  5
+
+import pytest 
+
+# Test 2 
+# def f(): 
+#     raise SystemExit(1)
+
+# def test_mytest():
+#     with pytest.raises(SystemExit):
+#         f()
+
+
+# Test 3
+
+# class TestClass:
+#     def test_one(self):
+#         x = "this"
+#         assert "h" in x
+    
+#     def test_two(self):
+#         x = "hello"
+#         assert hasattr(x, "check")
+
+# Test 3
+class TestClassDemoInstance: 
+    def test_one(self):
+        assert 0 
+    
+    def test_two(self):
+        assert 0
