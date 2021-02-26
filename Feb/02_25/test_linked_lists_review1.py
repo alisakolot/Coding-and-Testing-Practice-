582776914f8434fda6ddb1016cb8058a927dec5e
Alisa Kolot <kolot.alisa@gmail.com>
2021-02-25 20:52:44 -0600

    secret message

--- Feb/02_25/test_linked_lists_review1.py
+++ Feb/02_25/test_linked_lists_review1.py
@@ -2,7 +2,7 @@ import pytest
 from linked_lists_review1 import * 
 
 class TestLinkedList:
-    
+
     def setup_method(self):
         self.prepared_linked_list = LinkedList()
         self.prepared_linked_list.insert(5)
@@ -10,3 +10,5 @@ class TestLinkedList:
         self.prepared_linked_list.insert(15)
 
         assert list(self.prepared_linked_list) == [5, 10, 15]
+
+# trolling github because i like the bright green squares
