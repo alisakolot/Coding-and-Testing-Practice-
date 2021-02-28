ff8b30e99c8b944b622bbf8f71a8194928b69afb
Alisa Kolot <kolot.alisa@gmail.com>
2021-02-27 20:58:17 -0600

    error1

--- Feb/02_27/check_parenthesis.py
+++ Feb/02_27/check_parenthesis.py
@@ -13,4 +13,34 @@ Constraints
 
 0 <= input.length <= 100
 
-Along with opening (() and closing ()) parenthesis, input may contain any valid ASCII characters. Furthermore, the input string may be empty and/or not contain any parentheses at all. Do not treat other forms of brackets as parentheses (e.g. [], {}, <>).
+Along with opening (() and closing ()) parenthesis, input 
+may contain any valid ASCII characters. Furthermore, the 
+input string may be empty and/or not contain any parentheses 
+at all. Do not treat other forms of brackets as parentheses 
+(e.g. [], {}, <>)."""
+
+def valid_parentheses(string):
+    lp = 0
+    rp = 0
+    
+    for char in string:
+        if char == '(':
+            lp += 1
+            li = string.index(char)
+            char = char[(li + 1):]
+        elif char == ')':
+            rp += 1
+            ri = string.index(char)
+            char = char[(ri + 1):]
+        else:
+            print(char)
+
+    if lp != rp:
+        print('not match')
+        return False
+    else:
+        print('match')
+        return True
+
+# Reason for error:
+# Parenthesis that do not face each other cannot count as a match
--- Feb/02_27/test_check_parenthesis.py
+++ Feb/02_27/test_check_parenthesis.py
@@ -1,1 +1,9 @@
-test_check_parenthesis.py
+import pytest
+from check_parenthesis import * 
+
+def test_valid_parenthesis():
+    assert valid_parentheses("  (") == False
+    assert valid_parentheses(")test") ==  False
+    assert valid_parentheses("") == True
+    assert valid_parentheses("hi())(") == False
+    assert valid_parentheses("hi(hi)()") == True
