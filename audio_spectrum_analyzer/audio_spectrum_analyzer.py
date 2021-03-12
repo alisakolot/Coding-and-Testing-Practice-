e77232a5869fbfd483654e43bf96f1b345e0a45c
Alisa Kolot <kolot.alisa@gmail.com>
2021-03-11 21:28:12 -0600

    fixing tk

--- audio_spectrum_analyzer/audio_spectrum_analyzer.py
+++ audio_spectrum_analyzer/audio_spectrum_analyzer.py
@@ -21,7 +21,7 @@ RATE = 44100
 p = pyaudio.PyAudio()
 # Create class instance, main pyaudio object 
 stream = p.open(
-    format=FORMAT, 
+    format = FORMAT, 
     channels = CHANNELS,
     rate = RATE, 
     input = True, 
@@ -30,12 +30,18 @@ stream = p.open(
 )
 # stream object
 
+'''GETTING BITS'''
 data = stream.read(CHUNK)
 # data object, when 'data' is run, you will get a bunch of bytes, but not integers  
     # can't plot in matplotlib, so you need to convert it 
     # [conversion using struct below]
 
-data_int = struct.unpack(str(2 * CHUNK) + 'B',  data)
+'''BITS TO INT'''
+# data_int = np.array(struct.unpack(str(2 * CHUNK) + 'B',  data), dtype = 'b')[::2] + 127
+# 'B': integer from 0 - 255
+# adding 127/half the range values that are greater than 255 will wrap back around 
+# [::2] : take every other point in array
+
 # print(len(data)) # length of data is double what our chunk is
 # print(data_int)# a tuple, int vals 0=255
 
@@ -43,10 +49,26 @@ data_int = struct.unpack(str(2 * CHUNK) + 'B',  data)
 # unpack: given string which signals how much data/size of data so 
 #   it knows how to unpack it
 #       => we pass the data: string
-
 fig, ax = plt.subplots ()
-ax.plot(data_int, '-')
-plt.show()
+
+x = np.arange(0, 2 * CHUNK, 2)
+line, = ax.plot(x, np.random.rand(CHUNK))
+ax.set_ylim(0, 255) 
+ax.set_xlim(0, CHUNK)
+
+
+while True: 
+
+    data = stream.read(CHUNK)
+    data_int = np.array(struct.unpack(str(2 * CHUNK) + 'B',  data), dtype = 'b')[::2] + 127
+
+    line.set_ydata(data_int)
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+
+
+# ax.plot(data_int, '-')
+# plt.show()
 
 # print(data)
 
