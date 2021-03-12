c8877a67d86e3028d990e7bac57e8b70c5d43061
Alisa Kolot <kolot.alisa@gmail.com>
2021-03-11 20:51:00 -0600

    data_int

--- audio_spectrum_analyzer/audio_spectrum_analyzer.py
+++ audio_spectrum_analyzer/audio_spectrum_analyzer.py
@@ -31,6 +31,25 @@ stream = p.open(
 # stream object
 
 data = stream.read(CHUNK)
-# data object, when 'data' is run, you will get a bunch of bytes  
-print(data)
+# data object, when 'data' is run, you will get a bunch of bytes, but not integers  
+    # can't plot in matplotlib, so you need to convert it 
+    # [conversion using struct below]
+
+data_int = struct.unpack(str(2 * CHUNK) + 'B',  data)
+# print(len(data)) # length of data is double what our chunk is
+# print(data_int)# a tuple, int vals 0=255
+# this obj will give us integer data instead of byte data
+# unpack: given string which signals how much data/size of data so 
+#   it knows how to unpack it
+#       => we pass the data: string
+
+
+# print(data)
+
+
+
+
+
+
+
 print("\n *** Program ran *** \n")
