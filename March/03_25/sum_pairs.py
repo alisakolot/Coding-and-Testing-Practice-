a91421191e38dc7cda7e6eee139479ea44995709
Alisa Kolot <kolot.alisa@gmail.com>
2021-03-25 15:45:06 -0500

    solution ideas

--- March/03_25/sum_pairs.py
+++ March/03_25/sum_pairs.py
@@ -3,7 +3,7 @@ return the first two values (parse from the left
 please) in order of appearance that add up to form the sum.
 
 Tests: 
-    >>>  sum_pairs([1, 4, 8, 7, 3, 15], 8)
+    >>> sum_pairs([1, 4, 8, 7, 3, 15], 8)
     [1, 7]
 
     >>> sum_pairs([1, -2, 3, 0, -6, 1], -6)
@@ -15,8 +15,17 @@ Tests:
 def sum_pairs(lst, n):
     result = []
     for i in range(len(lst)-1):
-        if lst[i] + lst[i+1] == n:
-            print('true')
+        pair_sum = lst[i]+lst[i+1] 
+        print(f'{lst[i]} + {lst[i+1]} = {pair_sum}')
+        if n == pair_sum:
+            result.append(lst[i])
+            result.append(lst[i+1])
+            print('ok')
+    return result
 
-    pass
-
+# Some solution ideas: 
+    # turn the given list into a stack?
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n You solved the thing! Here's a star! ⭐ \n")
